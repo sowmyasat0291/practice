04c017a2c0c425306a2af2cbd5a3ea4f9e72b557
Sowmya <sowmyam0291@gmail.com>
2024-09-12 10:06:33 +0530

    Test Commit

--- config/config/settings.py
+++ config/config/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework_simplejwt'
+    'rest_framework_simplejwt',
+    'app'
 ]
 
 MIDDLEWARE = [
